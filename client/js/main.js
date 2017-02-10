5786bc2725251670fbd4e2933a34732a394fcb7a
yourfriendlyneighbor <carlperelman1960@gmail.com>
2017-02-10 16:56:58 -0500

    updated

--- client/js/main.js
+++ client/js/main.js
@@ -16,8 +16,12 @@ $(function(){
   let cookies = 0;
   let upgrade_1_price = 25
   let upgrade_1_amount = 0;
+  let upgrade_2_price = 12;
+  let upgrade_2_amount = 0;
+  let cpc = 1;
   let cps = 0;
   $("#upgrade_1_price").html(upgrade_1_price);
+  $("#upgrade_2_price").html(upgrade_2_price);
   $("#cps").html(cps)
   $usersOnline.hide();
   $gameArea.hide();
@@ -35,11 +39,11 @@ $(function(){
 
   $($cookie).on( "click", function() {
     socket.emit('get username 4 click')
-    socket.emit('plus click', 1)
+    socket.emit('plus click', cpc)
   });
   socket.on('get username 4 click',function(data){
     alertify.warning(data + ": has clicked the cookie!", 1.25);
-  })
+  });
   socket.on('get clicks', function(data){
     cookies = data
     $cookies.html(data);
@@ -55,24 +59,50 @@ $(function(){
       socket.emit('upgrade 1 amount', upgrade_1_amount)
       cps++;
       socket.emit('cps', cps);
-      socket.emit('get username')
+      socket.emit('get username');
+      $("#amount_upgrade_1").html(upgrade_1_amount)
     }
   });
+  $($upgrade2).on('click', function(){
+    if(cookies >= upgrade_2_price){
+      cookies = cookies - upgrade_2_price;
+      socket.emit('remove cookies', upgrade_2_price);
+      upgrade_2_price = Math.floor(upgrade_2_price * 1.2)
+      socket.emit('upgrade 2 price', upgrade_2_price);
+      upgrade_2_amount++;
+      socket.emit('upgrade 2 amount',upgrade_2_amount);
+      cpc++;
+      socket.emit('cpc', cpc);
+      socket.emit('get username');
+      $("#amount_upgrade_2").html(upgrade_2_amount)
+    }
+  })
+
+
   socket.on('recieve username', function(data){
-    alertify.warning(data + ": has bought an item", 5);
+    alertify.success(data + ": has bought an item", 5);
   })
   socket.on('get cookies', function(data){
     $cookies.html(data);
-    console.log(data);
   });
   socket.on('get upgrade 1 price', function(data){
     upgrade_1_price = data;
     $("#upgrade_1_price").html(data);
-    console.log(data);
   });
   socket.on('get upgrade 1 amount', function(data){
     upgrade_1_amount = data;
-    console.log(data);
+    $("#amount_upgrade_1").html(upgrade_1_amount)
+  });
+  socket.on('get upgrade 2 price', function(data){
+    upgrade_2_price = data;
+    $("#upgrade_2_price").html(data);
+  });
+  socket.on('get upgrade 2 amount', function(data){
+    upgrade_2_amount = data;
+    $("#amount_upgrade_2").html(upgrade_2_amount)
+  });
+  socket.on('get cpc', function(data){
+    cpc = data;
   });
   socket.on('get cps', function(data){
     cps = data;
@@ -82,7 +112,6 @@ $(function(){
       cookies = cookies + cps;
       socket.emit('plus click', cps)
       $cookies.html(cookies);
-      console.log(cookies);
     }, 1000);
 
     /* Chat */
@@ -92,11 +121,9 @@ $(function(){
       $("#message").val(" ");
     })
     socket.on('new message', function(data){
-      console.log(data.msg);
       $("#chat").append('<div class="well"><strong>'+data.user + ': </strong>'+data.msg+'</div>');
     });
     socket.on('get users', function(data){
-      console.log(data)
       var html = '';
       for (var i = 0; i < data.length; i++) {
         html += '<li class="list-group-item">'+data[i]+'</li>'
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ messages = [];
 let clicks = 0;
 let upgrade_1_price = 25;
 let upgrade_1_amount = 0;
+let upgrade_2_price = 12;
+let upgrade_2_amount = 0;
+let cpc = 1;
 let cps = 0;
 
 const port = process.env.PORT || 3000;
@@ -30,28 +33,30 @@ io.sockets.on('connection', function(socket){
     connections.splice(connections.indexOf(socket), 1);
   });
 
-  //New User
-  socket.on('new user', function(data, callback){
-    callback(true);
-    socket.username = data;
-    users.push(socket.username);
-    updateUsernames();
-    io.emit('get username', socket.username);
-  });
+  /* USERNAME STUFF */
+    socket.on('new user', function(data, callback){
+      callback(true);
+      socket.username = data;
+      users.push(socket.username);
+      updateUsernames();
+      io.emit('get username', socket.username || "Guest");
+    });
 
-  function updateUsernames(){
-    io.emit('get users', users);
-  }
+    function updateUsernames(){
+      io.emit('get users', users);
+    }
 
+  /* CLICKS */
   socket.on('plus click', function(data){
     clicks = clicks + data;
     sendClicks()
   });
+  /* GETTING THINGS */
   socket.on('get username',function(){
-    io.emit('recieve username', socket.username)
+    io.emit('recieve username', socket.username || "Guest")
   });
   socket.on('get username 4 click',function(){
-    io.emit('get username 4 click', socket.username)
+    io.emit('get username 4 click', socket.username || "Guest")
   });
   function sendClicks(){
     io.emit('get clicks', clicks);
@@ -60,6 +65,7 @@ io.sockets.on('connection', function(socket){
     clicks = clicks - data;
     io.emit('get cookies', clicks);
   });
+  /* Increasing & Decreasing things */
   socket.on('upgrade 1 price', function(data){
     upgrade_1_price = data;
     io.emit('get upgrade 1 price', upgrade_1_price)
@@ -67,6 +73,20 @@ io.sockets.on('connection', function(socket){
   socket.on('upgrade 1 amount', function(data){
     upgrade_1_amount = data;
     io.emit('get upgrade 1 amount', upgrade_1_amount)
+  });
+
+
+  socket.on('upgrade 2 price', function(data){
+    upgrade_2_price = data;
+    io.emit('get upgrade 2 price', upgrade_2_price)
+  });
+  socket.on('upgrade 2 amount', function(data){
+    upgrade_2_amount = data;
+    io.emit('get upgrade 2 amount', upgrade_2_amount)
+  });
+  socket.on('cpc', function(data){
+    cpc = data;
+    io.emit('get cpc', cpc)
   });
   socket.on('cps', function(data){
     cps = data;
@@ -78,19 +98,20 @@ io.sockets.on('connection', function(socket){
     io.emit('get cookies', clicks);
     io.emit('get upgrade 1 price', upgrade_1_price);
     io.emit('get upgrade 1 amount', upgrade_1_amount);
-    io.emit('get cps', cps)
+    io.emit('get upgrade 2 price', upgrade_2_price);
+    io.emit('get upgrade 2 amount', upgrade_2_amount);
+    io.emit('get cps', cps);
+    io.emit('get cpc', cpc)
   }
   startUp()
   //Chat
-  function updateUsernames(){
-    console.log(users)
-    io.emit('get users', users);
-  }
-  //Send Message
-  socket.on('send message', function(data){
-    messages.push(data)
-    console.log(messages);
-    var randomGuest = Math.floor(Math.random() * (10000 - 1) + 1);
-    io.emit('new message', {msg: data, user: socket.username || "Guest "})
-  })
+    function updateUsernames(){
+      io.emit('get users', users);
+    }
+    //Send Message
+    socket.on('send message', function(data){
+      messages.push(data)
+      var randomGuest = Math.floor(Math.random() * (10000 - 1) + 1);
+      io.emit('new message', {msg: data, user: socket.username || "Guest "})
+    })
 })
